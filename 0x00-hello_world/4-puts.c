9e8d368d98aebdc013ab03ad5173a824880d6adc
timidgeek <5568@holbertonstudents.com>
2022-09-17 10:55:24 -0700

    compile

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,2 @@
 #!/bin/bash
-char st[] = "\"Programming is like building a multilingual puzzle"; puts str; return 0
+gcc char st[] = "\"Programming is like building a multilingual puzzle"; puts str; return 0
