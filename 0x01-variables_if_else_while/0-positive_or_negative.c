d1c07cb48da4e2372f7cbedbb580a9d0f7ca6fc6
timidgeek <5568@holbertonstudents.com>
2022-09-19 13:09:20 -0700

    posis and negis

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,36 +1,22 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - Assigning random numbers
+ *
+ * Return: Always 0 (success)
+*/
 int main(void)
 {
-		int n;
+	int n;
 
-			srand(time(0));
-				n = rand() - RAND_MAX / 2;
-					int n = 0
-					while(n > 0)
-					{
-						printf("%d is positive\n", n > 0);
-						n++;
-					}
-					while(n == 0)
-					{
-						printf("%d is zero\n", n == 0);
-						n++;
-					}
-					while(n < 0)
-						printf("%d is negative\n", n < 0);
-						n++;
-					}
-					return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive\n", n > 0);
+	else if (n == 0)
+		printf("%d is zero\n", n == 0);
+	if (n < 0)
+		printf("%d is negative\n", n < 0);
+	return (0);
 }
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-
