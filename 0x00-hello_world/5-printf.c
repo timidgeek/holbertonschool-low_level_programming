6c6bfabd2ba414fad99c4b304a4eff44c0f1acfa
timidgeek <5568@holbertonstudents.com>
2022-09-19 08:17:00 -0700

    hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,10 @@
 /**
  * main - Printing a statement
  *
- * Return: Alawys 0 (success)
+ * Return: Always 0 (success)
  */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,")\n;
+	printf("with proper grammar, but the outcome is a piece of art,"\n);
 	return(0);
 }
