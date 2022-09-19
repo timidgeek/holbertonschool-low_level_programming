3974ca019939ee3faab470c597fa0599fd0f1eff
timidgeek <5568@holbertonstudents.com>
2022-09-19 09:22:18 -0700

    grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,23 +8,6 @@
 
 int main(void)
 {
-	printf("signed char: ");
-	printSignedRange(sizeof(char));
-
-	printf("unsigned char: ");
-	printSignedRange(sizeof(unsigned char));
-
-	printf("signed int: ");
-	printSignedRange(sizeof(int));
-
-	printf("unsigned int: ");
-	printSignedRange(sizeof(unsigned char));
-
-	printf("signed short int: ");
-	printSignedRange(sizeof(short int));
-
-	printf("unsigned short int: ");
-	printSignedRange(sizeof(unsigned short int));
-
+	printf("The size of integer is %u\n" sizeof(n));
 	return (0);
 }
