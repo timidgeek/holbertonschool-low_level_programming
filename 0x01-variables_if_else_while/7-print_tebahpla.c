9f7546b94215008ee75fe06141aae045f9f7ab3d
timidgeek <5568@holbertonstudents.com>
2022-09-20 11:57:02 -0700

    smile

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Alphabet in reverse
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+char soup;
+
+for (soup = 'z'; soup <= 'a'; soup++)
+	putchar(soup);
+
+putchar('\n');
+
+return (0);
+}
