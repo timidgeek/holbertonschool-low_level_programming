cce5e8270bbf953dba11de47ca1cb85c38cbfa6b
timidgeek <5568@holbertonstudents.com>
2022-09-20 09:57:56 -0700

    curly braces

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Printing the lowercase alphabet using putchar
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+char soup;
+
+for (soup = 'a'; soup <= 'z'; soup++)
+{
+	putchar(soup);
+	for (soup = 'A'; soup <= 'Z'; soup++)
+	putchar(soup);
+
+putchar('\n');
+}
+return (0);
+}
