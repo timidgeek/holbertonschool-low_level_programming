bc2d5bc58267caa411ee588b4f33cd65b4867a1f
timidgeek <5568@holbertonstudents.com>
2022-09-20 13:36:39 -0700

    number sequences

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - creating all possible combinations of single-digit numbers
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+int num;
+
+for (num = 0; num <= 9; num++)
+{	putchar ((num % 10) + '0');
+	putchar (',');
+	putchar(' ');
+}
+
+putchar('\n');
+
+return (0);
+}
