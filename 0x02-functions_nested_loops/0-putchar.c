6dbb765ea43892f996141cc13a2cddf6c65bd08c
timidgeek <5568@holbertonstudents.com>
2022-09-20 15:51:35 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - printing a message
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+C programming functions and nested loops.
