0ad147edcb3c88a859cbc48ace23c2529594da41
timidgeek <5568@holbertonstudents.com>
2022-09-20 11:30:52 -0700

    putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Using putchar to display numbers
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+char numb;
+
+for (numb = 0; numb < 10; numb++)
+	putchar(numb);
+putchar('\n');
+return (0);
+}
