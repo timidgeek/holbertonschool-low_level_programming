ebed8d9bb7fa9df8491f39e88a8d7b7a1d64bbca
timidgeek <5568@holbertonstudents.com>
2022-09-21 13:26:51 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,9 +1,15 @@
 #include "main.h"
 /**
  * print_last_digit - prints the last digit of a number
+ * @lastDig: variable
  *
  * Return: 0.
 */
-int print_last_digit(int)
+int print_last_digit(int lastDig)
 {
+	if (lastDig % 10)
+	{
+		_putchar(lastDig % 10);
+	}
+return (0);
 }
