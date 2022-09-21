0603954de89d272c2a82effab2ad5472a7b2e558
timidgeek <5568@holbertonstudents.com>
2022-09-21 08:51:09 -0700

    return 0

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,24 +3,25 @@
  * print_sign - prints the sign of a number
  * @n: number check
  *
- * Return: 0.
+ * Return: 0 if n == 0, 1 if n > 0, -1 if n < 0.
 */
+
 int print_sign(int n)
 {
-if (n > 0)
-{
-	printf("+");
-	return (1);
-}
-if (n == 0)
-{
-	printf("0");
+	if (n > 0)
+	{
+		printf("+");
+		return (1);
+	}
+	if (n == 0)
+	{
+		printf("0");
+		return (0);
+	}
+	if (n < 0)
+	{
+		printf("-");
+		return (-1);
+	}
 	return (0);
-}
-if (n < 0)
-{
-	printf("-");
-	return (-1);
-}
-return (0);
 }
