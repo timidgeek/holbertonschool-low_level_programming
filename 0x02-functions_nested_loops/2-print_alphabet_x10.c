63c12f830169a05d39bf28dedbbd8c6fdf9d0636
timidgeek <5568@holbertonstudents.com>
2022-09-20 19:42:50 -0700

    10 x betty

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,22 @@
 #include "main.h"
 
 /**
- * main - print alphabet in lowercase
+ * print_alphabet_x10 - print alphabet in lowercase ten times
  *
  * Return: Always 0.
  */
-int main(void)
+void print_alphabet_x10(void)
 {
-    print_alphabet();
-    return (0);
+	char betty;
+	int counter;
+
+	for (counter = 1; counter <= 10; counter++)
+	{
+		for (betty = 'a'; betty <= 'z'; betty++)
+		{
+			_putchar(betty);
+		}
+	_putchar('\n');
+	}
+
 }
