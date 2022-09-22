1412e69e1760a74a2d8b030d9e008766eda12dee
timidgeek <5568@holbertonstudents.com>
2022-09-21 19:10:28 -0700

    times table

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,4 +6,22 @@
 */
 void jack_bauer(void)
 {
+	int min = 0;
+	int hour = 0;
+
+	while (hour < 24)
+	{
+		while (min < 60)
+		{
+			_putchar('0' + (hour / 10));
+			_putchar('0' + (hour % 10));
+			_putchar(':');
+			_putchar('0' + (min / 10));
+			_putchar('0' + (min % 10));
+			_putchar('\n');
+			min++;
+		}
+		min = 0;
+		hour++;
+	}
 }
