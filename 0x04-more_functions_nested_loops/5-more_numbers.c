4c4829314b11966f543666327e75c1059c424935
timidgeek <5568@holbertonstudents.com>
2022-09-24 20:29:33 -0700

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,23 +3,20 @@
 /**
  * more_numbers - print a series of numbers ten times
  *
- * Return: 0
 */
 
 void more_numbers(void)
 {
 	int counter, num;
 
-	for (counter = 0; counter <= 9; counter++)
+	for (counter = 0; counter < 10; counter++)
     	{
         	for (num = 0; num <= 14; num++)
         	{
-			if (counter > 9)
-			{
-				putchar('0' + (num / 10));
-				putchar('0' + (num % 10));
-			}
+			if (num > 9)
+				putchar((num / 10) + '0');
+			putchar((num % 10) + '0');
 		}
+		putchar('\n');
 	}
-	putchar('\n');
 }
