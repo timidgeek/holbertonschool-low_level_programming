13ebb72b46f1a7018b1b72ded9e93b9e52c732d4
timidgeek <5568@holbertonstudents.com>
2022-09-24 17:56:48 -0700

    we don't like 2 or 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print selective numbers
+ *
+ * Return: 0
+*/
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			putchar(i + '0');
+		}
+		else
+		{
+		}
+	}
+	putchar('\n');
+}
