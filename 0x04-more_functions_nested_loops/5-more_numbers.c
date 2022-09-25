9b21ef4363ebe786c2165f860e3553f4a775ad71
timidgeek <5568@holbertonstudents.com>
2022-09-24 20:17:45 -0700

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,12 +10,15 @@ void more_numbers(void)
 {
 	int counter, num;
 
-	for (counter = 1; counter <= 10; counter++)
-	{
-		for (num = 0; num <= 14; num++)
-		{
-			putchar('0' + (num / 10));
-			putchar('0' + (num % 10));
+	for (counter = 0; counter <= 9; counter++)
+    	{
+        	for (num = 0; num <= 14; num++)
+        	{
+			if (counter > 9)
+			{
+				putchar('0' + (num / 10));
+				putchar('0' + (num % 10));
+			}
 		}
 	}
 	putchar('\n');
