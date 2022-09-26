b7d1de9364120100d8d629d1bc27db5dd0d2e498
timidgeek <5568@holbertonstudents.com>
2022-09-25 21:54:37 -0700

    alt-j

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle
+ * @size: size of triangle
+*/
+
+void print_triangle(int size)
+{
+	int x;
+	int y;
+	int z;
+
+	if (size > 0)
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (y = size - x; y > 1; y--)
+				_putchar(32);
+			for (z = 0; z <= x; z++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+}
--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,25 +14,15 @@ int main(void)
 	for (num = 1; num <= 100; num++)
 	{
 		if (num % 15 == 0)
-		{
 			printf("FizzBuzz");
-		}
 		else if (num % 5 == 0)
-		{
 			printf("Buzz");
-		}
 		else if (num % 3 == 0)
-		{
 			printf("Fizz");
-		}
 		else
-		{
 			printf("%d", num);
-		}
 		if (num != 100)
-		{
 			printf(" ");
-		}
 	}
 	putchar('\n');
 	return (0);
