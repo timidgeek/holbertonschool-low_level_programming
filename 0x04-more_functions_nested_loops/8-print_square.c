dd44d55aedb7295ecf99cc8b3fd10d853c838be5
timidgeek <5568@holbertonstudents.com>
2022-09-25 20:05:17 -0700

    hip to be square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square
+ * @size: variable
+*/
+
+void print_square(int size)
+{
+	int height;
+	int width;
+
+	if (size > 0)
+	{
+		for (height = 0; height < size; height++)
+		{
+			for (width = 0; width < size; width++)
+				_putchar('#');
+
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
