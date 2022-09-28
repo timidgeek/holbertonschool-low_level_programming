09f9c498af0f93ebea767770e400787ac95620fa
timidgeek <5568@holbertonstudents.com>
2022-09-28 10:05:04 -0700

    puts half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,15 +11,18 @@ void puts_half(char *str)
 	int start;
 
 	len = 0;
-	while (str[len++] != '\0')
+	while (str[len] != '\0')
 	{
-		;
+		len++;
 	}
 	if (len % 2 == 0)
 	{
-		len--;
+		start = len / 2;
 	}
-	start = len / 2;
+	if (len % 2 != 0)
+	{
+		start = ((len - 1) / 2) + 1;
+	}
 	while (str[start] != '\0')
 	{
 		_putchar(str[start]);
