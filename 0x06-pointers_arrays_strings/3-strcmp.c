705a5d8d2721c9e1dc850105883c8b987bbd7f39
timidgeek <5568@holbertonstudents.com>
2022-09-30 13:34:03 -0700

    compare strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,24 +4,23 @@
 * _strcmp - compares two strings
 * @s1: character
 * @s2: character
-* Return: 0 if flag == 0, else return 1
+* Return: 0 if flag == 0
 */
 int _strcmp(char *s1, char *s2)
 {
-	int flag = 0;
 	int i = 0;
 
-	while (s1[i] != '\0' && s2[i] != '\0')
+	while (*s1 != '\0' && *s2 != '\0')
 	{
-		if (s1[i] != s2[i])
+		if (*s1 != *s2)
 		{
-			flag = 1;
-			break;
+			return (*s1 - *s2);
+		}
+		else
+		{
+			return (0);
 		}
 		i++;
 	}
-	if (flag == 0)
-		return (0);
-	else
-		return (1);
+return (0);
 }
