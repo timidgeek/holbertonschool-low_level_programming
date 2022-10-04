d58e91df07e8c6111a45ef2257cd018b81449dc8
timidgeek <5568@holbertonstudents.com>
2022-10-04 13:39:40 -0700

    starbuck

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include "_strchr.c"
+
+/**
+* _strpbrk - searches a str for bytes
+* @s: string
+* @accept: string
+*
+* Return: to matching byte or NULL
+*/
+char *_strpbrk(char *s, char *accept)
+{
+	if ((*s == '\0') || (*accept == '\0'))
+		return ('\0');
+	while (*s)
+	{
+		if (_strchr(accept, *s))
+		{
+			return (s);
+		}
+		else
+		{
+			s++;
+		}
+	}
+	return ('\0');
+}
