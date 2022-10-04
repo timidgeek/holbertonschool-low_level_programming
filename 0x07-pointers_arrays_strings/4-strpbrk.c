276be0dee04b17ceb3a1eec098d1919aae9dbcda
timidgeek <5568@holbertonstudents.com>
2022-10-04 14:02:54 -0700

    lt. starbuck

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,11 +10,9 @@
 */
 char *_strpbrk(char *s, char *accept)
 {
-	if ((*s == '\0') || (*accept == '\0'))
-		return ('\0');
 	while (*s)
 	{
-		if (_strchr(*accept, *s))
+		if (_strchr(accept, *s))
 		{
 			return ((char *) s);
 		}
