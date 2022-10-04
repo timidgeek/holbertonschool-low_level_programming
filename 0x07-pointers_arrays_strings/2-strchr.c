66fedc1d2a86d142f69c1617b20fe714622e4cd0
timidgeek <5568@holbertonstudents.com>
2022-10-04 12:31:54 -0700

    starryeye

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,11 +9,12 @@
 */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	int i;
+
+	for (i = 0; s[i] >= '\0'; i++)
 	{
-		if (*s == c)
-			return ((char *) s);
-		s++;
+		if (s[i] == c)
+			return (s + i);
 	}
 	return ('\0');
 }
