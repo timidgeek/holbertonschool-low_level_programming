8a6c7698528c33daffd02adc7dca2496bac6d204
timidgeek <5568@holbertonstudents.com>
2022-10-10 17:31:20 -0700

    pushing header, readme, first task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* main - prints program's name
+* @argc: integer
+* @argv: array of strings
+* Return: name of program
+*/
+int main(int argc, char *argv[])
+{
+	argc = 0;
+
+	printf("%s\n", argv[argc]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* prototypes */
+
+int _putchar(char c);
+int main(int argc, char* argv[]);
+
+#endif
