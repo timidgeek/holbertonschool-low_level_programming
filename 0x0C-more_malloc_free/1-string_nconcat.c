1cf99a18df9a5b0a8fd32f60383a3c65b995d681
timidgeek <5568@holbertonstudents.com>
2022-10-19 11:39:57 -0700

    two comparisons for n & len2

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,4 +1,6 @@
 #include "main.h"
+int _strlen(char *s);
+
 /**
 * string_nconcat - concatenates two strings
 * @s1: string one
@@ -9,17 +11,20 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int i, x;
-	unsigned int len1 = strlen(s1);
-	unsigned int len2 = strlen(s2);
+	unsigned int len1 = _strlen(s1);
+	unsigned int len2 = _strlen(s2);
 	char *s, *check;
 
+	check = malloc(5);
 	if (s1 == NULL)
-		s1 = 0;
+		len1 = 0;
 	if (s2 == NULL)
-		s2 = 0;
-	if (n >= len2)
+		len2 = 0;
+	if (n >= len2) /* first n bytes of len2  */
 		n = len2;
-	s = malloc(len1 + n + 1);
+	if (n <= len2)
+		len2 = n;
+	s = malloc(sizeof(char) * (len1  + n + 1));
 	if (s == NULL)
 		return (NULL);
 	for (i = 0; i < len1; i++) /* finding length of s1  */
@@ -27,8 +32,22 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (x = 0; x < len2; x++)
 		s[x + i] = s2[x]; /* shift and copy  */
 	s[x + i] = '\0';
-	check = malloc(5);
 	free(check);
 
 	return (s);
+}
+/**
+ * _strlen - return length of string
+ * @s: character
+ * Return: length of string
+*/
+
+int _strlen(char *s)
+{
+	int y;
+
+	for (y = 0; s[y] != '\0'; ++y)
+	{
+	}
+	return (y);
 }
