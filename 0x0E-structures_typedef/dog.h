245f5c6fbfd303a7f1eca52d25bb86a641c8f79a
timidgeek <5568@holbertonstudents.com>
2022-10-24 11:21:53 -0700

    editing betty comments

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,18 +7,18 @@
 
 /* structures */
 
+/**
+* struct dog - puppy stuff
+* @name: dog name
+* @age: dog age
+* @owner: dog owner
+*/
+
 struct dog
-{ 
+{
 	char *name;
 	float age;
 	char *owner;
 };
-
-/* prototypes */
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 
 #endif
