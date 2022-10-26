c57e6ad0122962b82b151d7daeeac10864bcb06e
timidgeek <5568@holbertonstudents.com>
2022-10-25 19:37:32 -0700

    adding arg library

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,18 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+/* libraries */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/* prototypes */
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+int _putchar(char c);
+
+#endif
