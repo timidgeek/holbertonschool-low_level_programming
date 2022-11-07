63e8a4df0e60f709534675269edca309aba166b7
timidgeek <5568@holbertonstudents.com>
2022-11-07 11:03:24 -0800

    pushing readme, header, first task

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+/**
+* print_list - prints all the elements of a list_t list
+* @h: pointer
+*
+* Return: Number of nodes upon sucess, null upon failure.
+*/
+size_t print_list(const list_t *h)
+{
+	int count = 0; /* count variable */
+
+	if (h == NULL)
+		return (0);
+
+	while (h != NULL) /* traverse linked list and maintain count */
+	{
+		if (h->str == NULL)
+		{
+			printf("[%d] %s\n", 0, "(nil)");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str); /* print total count */
+		}
+		h = h->next;
+		count++;
+		
+	}
+
+	return (count);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,10 @@
+0x13. C - Singly linked lists
+
+Learning Objectives:
+	Know when and why to use linked lists vs arrays
+	Know how to build and use linked lists
+
+General Requirements:
+	Compilatioon using "gcc -Wall -Werror -Wextra -pedantic -std=gnu89"
+	Betty style, as always
+	The only standard library functions allowed are malloc, free, and exit
