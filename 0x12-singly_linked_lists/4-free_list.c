28e33a08623caf82337bca1adee7e79fc3e2d381
timidgeek <5568@holbertonstudents.com>
2022-11-07 14:34:51 -0800

    free them all

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+* free_list - frees list_t
+* @head: pointer to beginning of list
+*
+* Return: 0
+*/
+void free_list(list_t *head)
+{
+	if (head == NULL)
+		return;
+
+	if (head->next != NULL)
+		free_list(head->next);
+
+	free(head);
+	free(head->str);
+}
