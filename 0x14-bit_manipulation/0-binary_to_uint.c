d9e60c8d8612aa8db5f4cc567e5f93d87d2805d9
timidgeek <5568@holbertonstudents.com>
2022-11-09 12:32:11 -0800

    deleted white space

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,33 +1,33 @@
 #include "main.h"
 /**
-* int binary_to_uint - convert binary number to unsigned int
+* binary_to_uint - convert binary number to unsigned int
 * @b: constant char
 *
 * Return: converted number, or 0
 */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int res = 0;
-	int base = 1, i = 0;
+	unsigned int i = 0, x = 0;
 
 	if (b == NULL)
 		return (0);
 
-	while (b[i + 1])
+	while (b[i])
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
+
+		if (b[i] == '0')
+		{
+			x = x << 1;
+		}
+		if (b[i] == '1')
+		{
+			x = (x << 1) + 1;
+		}
+
 		i++;
 	}
-
-	while (i >= 0)
-	{
-		res += ((b[i] - '0') * base);
-		base *= 2;
-		i--;
-	}
-
-
-	return (res);
+	return (x);
 
 }
